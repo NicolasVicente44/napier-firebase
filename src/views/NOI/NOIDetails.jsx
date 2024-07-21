812dcc7dccccc400a5868f9ecd3c8589d7428eae
Your Name <your_email@example.com>
2024-07-21 17:10:37 -0400

    fixed header on details page

--- src/views/NOI/NOIDetails.jsx
+++ src/views/NOI/NOIDetails.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Header from "../components/Header";
+import EmptyHeader from "../components/EmptyHeader";
 import Sidebar from "../components/Sidebar";
 import {
   fetchNoiById,
@@ -123,7 +123,7 @@ const NOIDetails = ({ user }) => {
       <ToastContainer />
       <Sidebar user={user} />
       <div className="flex-1 flex flex-col overflow-hidden md:ml-64 lg:ml-80">
-        <Header />
+        <EmptyHeader />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
           <div className="container mx-auto px-6 py-8">
             <div className="bg-white p-6 rounded-lg shadow-md relative">
